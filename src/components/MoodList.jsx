27511d8e1a0eda940e428303fec24edd6fd10e04
Jay Joshi <jayjoshi82694@gmail.com>
2026-06-10 18:55:19 +0000

    img change

--- src/components/MoodList.jsx
+++ src/components/MoodList.jsx
@@ -22,7 +22,7 @@ const moods = [
   { emoji: "😯", label: "Surprise",image:"https://www.gif-vif.com/desi_gifs/sleepy-jethalal-taarak-mehta-ka-ooltah-chashmah-aa4297952e.gif" ,contanct : "--Fafda Jalebi --"} ,
   { emoji: "😯", label: "Fear",image:"https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2019/07/pjimage-1-1562172515.jpg",contanct : "--Bhootni --" },
   { emoji: "😯", label: "Dance",image:"https://media.tenor.com/oZp1QzbgX4AAAAAM/dance.gif" ,contanct : "--ayeee Hallooooooo--" },
-  { emoji: "😯", label: "Confused",image:"https://media.licdn.com/dms/image/v2/C4D22AQEyXDYckLBeNw/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1663304034578?e=2147483647&v=beta&t=Q7pR_m_ytM0eASSMc4ooclstekBU7m77wkB8GjIe3Qo",contanct : "--Sunder Itne le gya toh kitne bache --" },
+  { emoji: "😯", label: "Confused",image:"https://media.tenor.com/QzJbUMt9krEAAAAM/jethalal-tmkoc.gif",contanct : "--Sunder Itne le gya toh kitne bache --" },
   { emoji: "😢", label: "Holi " ,image:"https://i.pinimg.com/originals/d6/60/26/d66026295d4abc84fbed451f38a1140f.gif" ,contanct : "--Inko dekho jitna nacha rhe--"},
   { emoji: "😢", label: "Fiting" ,image:"https://media.tenor.com/r6xwrsc8l_oAAAAM/bday-fight.gif" ,contanct : "--Babita Ji ke Liye Me acha Gift laya hun --"},
 
